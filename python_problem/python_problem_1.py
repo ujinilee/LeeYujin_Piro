9825443324a4a66f674569996a9f4f48d744b0cc
Yujin Lee <hojen5589@naver.com>
2022-01-06 01:25:27 +0900

    1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,34 +1,39 @@
+import random
+
 def brGame():
     num=0
     score=0
-    playerA=True
+    player=True
 
     while(score!=31):
-        while(1):    
-            num=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능):")
+        while(1):
+            if(player==True):
+                num=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능):")
             
-            if(num.isnumeric()!=True):
+            if(player==True and num.isnumeric()!=True):
                 print("정수를 입력하세요")
-            elif(int(num)!=1 and int(num)!=2 and int(num)!=3):
+            elif(player==True and int(num)!=1 and int(num)!=2 and int(num)!=3):
                 print("1,2,3 중 하나를 입력하세요")    
             else:
+                if(player==False):
+                    num=random.randint(1,3)
                 for i in range(1,int(num)+1):
                     score+=1
                     if(score==31):
-                        if(playerA==True):
-                            print('playerA : ',score)
-                            print("playerB win!")
+                        if(player==True):
+                            print('player : ',score)
+                            print("computer win!")
                         else:
-                            print('playerB : ',score)
-                            print("playerA win!")
+                            print('computer : ',score)
+                            print("player win!")
                         break
                     
-                    if(playerA==True):
-                        print('playerA : ',score)
+                    if(player==True):
+                        print('player : ',score)
                     else:
-                        print('playerB : ',score)
+                        print('computer : ',score)
                 
-                playerA=not playerA
+                player=not player
                 break
             
         if(score==31):
