3d3d83441e6a5ff29370b00f284f9844f8fdd8e3
Yujin Lee <hojen5589@naver.com>
2022-01-06 00:21:30 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,5 @@
 num=0
+score=0
 
 while(1):    
     num=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능):")
@@ -9,4 +10,21 @@ while(1):
         print("1,2,3 중 하나를 입력하세요")    
     else:
         for i in range(1,int(num)+1):
-            print('playerA : ',i)
+            score+=1
+            print('playerA : ',score)
+    
+    break
+
+while(1):    
+    num=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능):")
+    
+    if(num.isnumeric()!=True):
+        print("정수를 입력하세요")
+    elif(int(num)!=1 and int(num)!=2 and int(num)!=3):
+        print("1,2,3 중 하나를 입력하세요")    
+    else:
+        for i in range(1,int(num)+1):
+            score+=1
+            print('playerB : ',score)
+    
+    break
