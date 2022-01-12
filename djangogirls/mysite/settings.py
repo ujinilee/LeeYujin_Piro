c0376e3e3fb690ea128b56b33e2f84ae270ae8c8
Yujin Lee <hojen5589@naver.com>
2022-01-12 16:44:49 +0900

    나의 첫 번째 Django 프로젝트!

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hpw)4t$qf0&n%-zl3%7$sn4ima-v=jnk(^)5_ix_+n4j6blqb%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
