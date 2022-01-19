371cf2371e35841d73318d0668a9ae9cc2fb0ee2
Yujin Lee <hojen5589@naver.com>
2022-01-20 03:35:09 +0900

    dev

--- SWIDEA_SITE/swidea/urls.py
+++ SWIDEA_SITE/swidea/urls.py
@@ -7,10 +7,22 @@ app_name='swidea'
 urlpatterns=[
     path('',views.idea_list,name="idea_list"), #리스트 페이지
     path('<int:pk>/',views.idea_detail,name='idea_detail'), #디테일페이지
+
     path('new/', views.idea_new, name='idea_new'), #생성
+
     path('<int:pk>/edit/',views.idea_edit,name='idea_edit'), ##수정
-    path('<int:pk>/delete', views.idea_delete, name='idea_delete') #삭제하기
+    path('<int:pk>/delete', views.idea_delete, name='idea_delete'), #삭제하기
 
 
-    path('dev/',views.dev_list, name="dev_list"),
+
+    path('dev/',views.dev_list, name="dev_list"), #리스트 페이지
+    path('dev/<int:pk>/',views.dev_detail,name='dev_detail'), #디테일페이지
+
+    path('dev/new/',views.dev_new,name='dev_new'), #생성
+
+
+    path('dev/<int:pk>/edit/',views.dev_edit,name='dev_edit'), ##수정
+    path('dev/<int:pk>/delete', views.dev_delete, name='dev_delete'), #삭제하기
+
+
 ]
--- SWIDEA_SITE/swidea/views.py
+++ SWIDEA_SITE/swidea/views.py
@@ -61,3 +61,50 @@ def dev_list(request):
     ctx = {'swideas': swideas}
 
     return render(request, template_name='dev_list.html', context=ctx)
+
+
+
+def dev_detail(request,pk):
+    swidea=Devtool.objects.get(id=pk)
+    ideas=Idea.objects.filter(devtool=pk)
+
+    ctx = {'swidea': swidea, 'ideas':ideas}
+
+    return render(request, 'dev_detail.html',context=ctx)
+
+
+
+
+def dev_new(request):
+    if request.method=='POST':
+        form=DevtoolForm(request.POST) 
+        if form.is_valid():
+            swidea=form.save() 
+            return redirect('swidea:dev_detail',pk=swidea.pk)
+
+    else: 
+        form=DevtoolForm()
+        ctx = {'form': form}
+        return  render(request,'idea_edit.html',context=ctx)
+
+
+
+
+def dev_edit(request, pk):
+    swidea = get_object_or_404(Devtool, pk=pk)
+    if request.method == "POST":
+        form = DevtoolForm(request.POST, instance=swidea)
+        if form.is_valid():
+            swidea.save()
+            return redirect('swidea:dev_detail', pk=swidea.pk)
+    else:
+        form = DevtoolForm(instance=swidea)
+    return render(request, 'dev_edit.html', {'form': form})
+
+
+
+
+def dev_delete(request,pk):
+    swidea = get_object_or_404(Devtool, id=pk)
+    swidea.delete()
+    return redirect('swidea:dev_list')
