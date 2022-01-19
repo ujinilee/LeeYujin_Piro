f0e9bb270a30679396eab65aeb037f4237745921
Yujin Lee <hojen5589@naver.com>
2022-01-20 00:20:29 +0900

    idea

--- SWIDEA_SITE/swidea/urls.py
+++ SWIDEA_SITE/swidea/urls.py
@@ -8,4 +8,7 @@ urlpatterns=[
     path('',views.idea_list,name="idea_list"),
     path('<int:pk>/',views.idea_detail,name='idea_detail'),
     path('new/', views.idea_new, name='idea_new'),
+    path('<int:pk>/edit/',views.idea_edit,name='idea_edit'), ##수정
+    path('<int:pk>/delete', views.idea_delete, name='idea_delete') #삭제하기
+
 ]
--- SWIDEA_SITE/swidea/views.py
+++ SWIDEA_SITE/swidea/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Devtool, Idea
-from .forms import IdeaForm
+from .forms import IdeaForm, DevtoolForm
 
 # Create your views here.
 
@@ -12,22 +12,43 @@ def idea_list(request):
     return render(request, template_name='idea_list.html', context=ctx)
 
 
+
+
 def idea_detail(request,pk):
-    swidea=get_object_or_404(Idea,pk=pk)
+    swidea=Idea.objects.get(id=pk)
     ctx = {'swidea': swidea}
     return render(request, 'idea_detail.html',context=ctx)
 
+
+
+    
+
 def idea_new(request):
     if request.method=='POST':
-        form=IdeaForm(request.POST) 
+        form=IdeaForm(request.POST, request.FILES) 
         if form.is_valid():
-            #poster=request.FILES.get('poster')
-            #Idea.idea_image=poster
             swidea=form.save() 
-
-            return redirect('swidea:idea_detail')
+            return redirect('swidea:idea_detail',pk=swidea.pk)
 
     else: 
         form=IdeaForm()
         ctx = {'form': form}
         return  render(request,'idea_edit.html',context=ctx)
+
+
+def idea_edit(request, pk):
+    swidea = get_object_or_404(Idea, pk=pk)
+    if request.method == "POST":
+        form = IdeaForm(request.POST, instance=swidea)
+        if form.is_valid():
+            swidea.save()
+            return redirect('swidea:idea_detail', pk=swidea.pk)
+    else:
+        form = IdeaForm(instance=swidea)
+    return render(request, 'idea_edit.html', {'form': form})
+
+
+def idea_delete(request,pk):
+    swidea = get_object_or_404(Idea, id=pk)
+    swidea.delete()
+    return redirect('swidea:idea_list')
